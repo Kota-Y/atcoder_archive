6cd6d44bd76e6501c1898f075f51fc23c642c781
kota0501 <mcz-0604-htt@outlook.com>
2020-01-13 18:03:31 +0900

    [AC] abc144 abc144_b

---
+++ atcoder.jp/abc144/abc144_b/Main.py
@@ -0,0 +1,12 @@
+def main():
+    n = int(input())
+
+    for i in range(1,10):
+        if n % i == 0 and n // i < 10:
+            print('Yes')
+            exit()
+
+    print('No')
+
+if __name__ == '__main__':
+    main()
