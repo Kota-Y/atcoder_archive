d53107d6ef447e948d1a39161a9e863f9163f4a8
kota0501 <mcz-0604-htt@outlook.com>
2020-01-19 16:19:20 +0900

    [AC] abc131 abc131_a

---
+++ atcoder.jp/abc131/abc131_a/Main.py
@@ -0,0 +1,12 @@
+def main():
+    s = input()
+
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            print('Bad')
+            exit()
+ 
+    print('Good')
+
+if __name__ == '__main__':
+    main()
