8fc8b92e15ee762d118a404a6bc05a61bb170167
kota0501 <mcz-0604-htt@outlook.com>
2020-02-12 00:09:46 +0900

    [AC] abc122 abc122_a

---
+++ atcoder.jp/abc122/abc122_a/Main.py
@@ -0,0 +1,14 @@
+def main():
+    b = input()
+
+    if b == 'A':
+        print('T')
+    elif b == 'T':
+        print('A')
+    elif b == 'C':
+        print('G')
+    elif b == 'G':
+        print('C')
+
+if __name__ == '__main__':
+    main()
