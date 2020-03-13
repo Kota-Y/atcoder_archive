947d3c0b1a512d9a717be62e644134edd5bb860e
kota0501 <mcz-0604-htt@outlook.com>
2020-03-14 02:05:06 +0900

    [AC] sumitrust2019 sumitb2019_a

---
+++ atcoder.jp/sumitrust2019/sumitb2019_a/Main.py
@@ -0,0 +1,11 @@
+def main():
+    m1, d1 = map(int, input().split())
+    m2, d2 = map(int, input().split())
+
+    if m1 != m2:
+        print('1')
+    else:
+        print('0')
+
+if __name__ == '__main__':
+    main()
