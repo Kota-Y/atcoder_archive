07f43b69bd49c89ccea73a4c43d74342a1583929
kota0501 <mcz-0604-htt@outlook.com>
2020-03-19 22:09:35 +0900

    [AC] arc054 arc054_b

---
+++ atcoder.jp/arc054/arc054_b/Main.py
@@ -0,0 +1,20 @@
+p = float(input())
+
+def f(x):
+    return x+p/2**(x/1.5)
+
+def main(): 
+    high = 100
+    low = 0
+    while high - low > 0.000000001:
+        mid_left = high/3+low*2/3
+        mid_right = high*2/3+low/3
+        if f(mid_left) >= f(mid_right):
+            low = mid_left
+        else:
+            high = mid_right
+
+    print(f(high))
+
+if __name__ == '__main__':
+    main()
