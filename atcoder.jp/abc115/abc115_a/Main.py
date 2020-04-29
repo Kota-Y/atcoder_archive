9952b005b6aa677e33c92758dc46d0d70f496fc2
kota0501 <mcz-0604-htt@outlook.com>
2020-04-29 21:52:06 +0900

    [AC] abc115 abc115_a

---
+++ atcoder.jp/abc115/abc115_a/Main.py
@@ -0,0 +1,14 @@
+def main():
+    n = int(input())
+
+    if n == 25:
+        print('Christmas')
+    elif n == 24:
+        print('Christmas Eve')
+    elif n == 23:
+        print('Christmas Eve Eve')
+    else:
+        print('Christmas Eve Eve Eve')
+
+if __name__ == '__main__':
+    main()
