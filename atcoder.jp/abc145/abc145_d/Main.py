7b870bfde9e19af7a807dee6c376bf67395879f9
kota0501 <mcz-0604-htt@outlook.com>
2020-05-14 21:53:07 +0900

    [AC] abc145 abc145_d

---
+++ atcoder.jp/abc145/abc145_d/Main.py
@@ -0,0 +1,59 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial#, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left
+from functools import reduce
+input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def Yes(): print('Yes')
+def No(): print('No')
+def YES(): print('YES')
+def NO(): print('NO')
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def mod_cmb(n: int, k: int, p: int) -> int:
+    if n < 0 or k < 0 or n < k: return 0
+    if n == 0 or k == 0: return 1
+    if (k > n - k):
+        return mod_cmb(n, n - k, p)
+    c = d = 1
+    for i in range(k):
+        c *= (n - i)
+        d *= (k - i)
+        c %= p
+        d %= p
+    return c * pow(d, p - 2, p) % p
+
+def main():
+    x, y = i_map()
+
+    if (x+y)%3 != 0:
+        print(0)
+        exit()
+
+    a = int((2*x-y) / 3)
+    b = int((-x+2*y) / 3)
+
+    if a < 0 or b < 0:
+        print(0)
+        exit()
+
+    print(mod_cmb(a+b,a,MOD))
+
+if __name__ == '__main__':
+    main()
