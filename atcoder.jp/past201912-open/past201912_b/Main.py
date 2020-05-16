5ddc13079bfc0d9bcb67121b46fd6e8d59e5686d
kota0501 <mcz-0604-htt@outlook.com>
2020-05-16 15:34:44 +0900

    [AC] past201912-open past201912_b

---
+++ atcoder.jp/past201912-open/past201912_b/Main.py
@@ -0,0 +1,45 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial#, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left
+from functools import reduce
+input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def Yes(): print('Yes')
+def No(): print('No')
+def YES(): print('YES')
+def NO(): print('NO')
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def main():
+    n = i_input()
+    num_list = i_row(n)
+
+    status = ['stay', 'up', 'down']
+
+    for i in range(1,n):
+        tmp = num_list[i] - num_list[i-1]
+        if tmp == 0:
+            print(status[0])
+        elif tmp > 0:
+            print(status[1], tmp)
+        else:
+            print(status[2], tmp*-1)
+
+if __name__ == '__main__':
+    main()
