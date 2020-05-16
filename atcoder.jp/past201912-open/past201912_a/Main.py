ab7808d798b7ecf945accbec5e7667a3726c9310
kota0501 <mcz-0604-htt@outlook.com>
2020-05-16 15:30:45 +0900

    [AC] past201912-open past201912_a

---
+++ atcoder.jp/past201912-open/past201912_a/Main.py
@@ -0,0 +1,47 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial#, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left
+from functools import reduce
+input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def Yes(): print('Yes')
+def No(): print('No')
+def YES(): print('YES')
+def NO(): print('NO')
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def main():
+    s = s_input()
+
+    d_list = [ str(i) for i in range(0,10)]
+
+    for i in range(3):
+        if s[i] not in d_list:
+            print('error')
+            exit()
+
+    calc_num = int(s)
+
+    if s[0] == '0':
+        calc_num = int(s[1:])
+
+    print(calc_num*2)
+
+if __name__ == '__main__':
+    main()
