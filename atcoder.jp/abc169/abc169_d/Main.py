a01586f5f8ff87ee5baff715f0a67c873f133ab3
kota0501 <mcz-0604-htt@outlook.com>
2020-05-31 22:11:13 +0900

    [AC] abc169 abc169_d

---
+++ atcoder.jp/abc169/abc169_d/Main.py
@@ -0,0 +1,81 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left
+from functools import reduce
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def Yes(): print('Yes')
+def No(): print('No')
+def YES(): print('YES')
+def NO(): print('NO')
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def prime_factorize(n: int) -> list:
+    return_list = []
+    while n % 2 == 0:
+        return_list.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            return_list.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        return_list.append(n)
+    return return_list
+
+def main():
+    n = i_input()
+
+    if n == 1:
+        print(0)
+        exit()
+
+    tmp = prime_factorize(n)
+
+    if len(tmp) == 1:
+        print(1)
+        exit()
+
+    count = 0
+
+    counter_tmp = Counter(tmp)
+
+    a_list = []
+
+    for k, v in counter_tmp.items():
+        while v >= 1:
+            a_list.append(k**v)
+            v -= 1
+
+    a_list.sort()
+
+    for a in a_list:
+        if n % a != 0:
+            continue
+        n /= a
+
+        count += 1
+
+    print(count)
+
+if __name__ == '__main__':
+    main()
