7c785a3daf16443d94acc3eb97bf6236d2fedbaf
kota0501 <mcz-0604-htt@outlook.com>
2020-05-31 21:08:17 +0900

    [AC] abc169 abc169_b

---
+++ atcoder.jp/abc169/abc169_b/Main.py
@@ -0,0 +1,52 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left
+from functools import reduce
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def Yes(): print('Yes')
+def No(): print('No')
+def YES(): print('YES')
+def NO(): print('NO')
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def main():
+    n = i_input()
+    num_list = i_list()
+
+    zero_c = num_list.count(0)
+
+    if zero_c >= 1:
+        print(0)
+        exit()
+
+    ans = 1
+
+    max_num = 10**18
+
+    for num in num_list:
+        ans *= num
+        if ans > max_num:
+            print(-1)
+            exit()
+
+    print(ans)
+            
+if __name__ == '__main__':
+    main()
