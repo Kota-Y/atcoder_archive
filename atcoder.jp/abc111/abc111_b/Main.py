901bbb947a297d7137cde5185398ed75aa3f578c
kota0501 <mcz-0604-htt@outlook.com>
2020-06-06 18:07:24 +0900

    [AC] abc111 abc111_b

---
+++ atcoder.jp/abc111/abc111_b/Main.py
@@ -0,0 +1,45 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial#, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def Yes(): print('Yes')
+def No(): print('No')
+def YES(): print('YES')
+def NO(): print('NO')
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def main():
+    sn = s_input()
+    n = int(sn)
+
+    compare_num = int(sn[0]) * 100 + int(sn[0]) * 10 + int(sn[0])
+    
+    if n == compare_num:
+        print(n)
+    elif n > compare_num:
+        tmp = int(sn[0]) + 1
+        print(tmp * 100 + tmp * 10 + tmp)
+    else:
+        print(compare_num)
+
+if __name__ == '__main__':
+    main()
