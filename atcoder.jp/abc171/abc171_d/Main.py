b19e89ae43c2fe3b81bff457924cac945bcb43c6
kota0501 <mcz-0604-htt@outlook.com>
2020-06-21 21:58:46 +0900

    [AC] abc171 abc171_d

---
+++ atcoder.jp/abc171/abc171_d/Main.py
@@ -0,0 +1,53 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def Yes(): print('Yes')
+def No(): print('No')
+def YES(): print('YES')
+def NO(): print('NO')
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    num_list = i_list()
+    q = i_input()
+    current_sum = sum(num_list)
+    cnt_list = Counter(num_list)
+    # print(cnt_list)
+    # print(current_sum)
+
+    # print('----')
+
+    for _ in range(q):
+        b, c = i_map()
+        b_cnt = cnt_list[b]
+        cnt_list[b] = 0
+        cnt_list[c] += b_cnt
+        current_sum += (c-b) * b_cnt
+        print(current_sum)
+
+
+if __name__ == '__main__':
+    main()
