b998ad50b67bbd8d7b3820dcd21de974b686e8f7
kota0501 <mcz-0604-htt@outlook.com>
2020-06-25 18:53:07 +0900

    [AC] abc100 abc100_c

---
+++ atcoder.jp/abc100/abc100_c/Main.py
@@ -0,0 +1,53 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def Yes(): print('Yes')
+def No(): print('No')
+def YES(): print('YES')
+def NO(): print('NO')
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def count_check(n):
+    result = 0
+    while n % 2 == 0:
+        n /= 2
+        result += 1
+    return result
+
+def main():
+    n = i_input()
+    num_list = i_list()
+
+    count = 0 
+
+    for num in num_list:
+        if num % 2 != 0:
+            continue
+        count += count_check(num)
+
+    print(count)
+ 
+if __name__ == '__main__':
+    main()
