fcea9698fb3d75e1221c6b23c545afb84241f145
kota0501 <mcz-0604-htt@outlook.com>
2020-06-28 15:16:55 +0900

    [AC] abc172 abc172_d

---
+++ atcoder.jp/abc172/abc172_d/Main.py
@@ -0,0 +1,44 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+
+    ans_list = [1] * n
+
+    for i in range(2,n+1):
+        for j in range(i,n+1,i):
+            ans_list[j-1] += 1
+
+    count = 0
+    for i, ans in enumerate(ans_list):
+        count += (i+1) * ans
+
+    print(count)
+ 
+if __name__ == '__main__':
+    main()
