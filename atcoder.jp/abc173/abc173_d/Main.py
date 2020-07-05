684cfd0710f82f4eddc5570239d3e15d520122a0
kota0501 <mcz-0604-htt@outlook.com>
2020-07-05 22:15:42 +0900

    [AC] abc173 abc173_d

---
+++ atcoder.jp/abc173/abc173_d/Main.py
@@ -0,0 +1,55 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_list(N): return [s_list() for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    num_list = i_list()
+
+    co_list = Counter(num_list)
+
+    count = 0
+
+    num_list = list(set(num_list))
+    num_list.sort(reverse=True)
+
+    ans_list = []
+
+    for i, num in enumerate(num_list):
+        num_c = co_list[num]
+
+        for _ in range(num_c*2):
+            ans_list.append(num)
+        if i == 0:
+            ans_list.pop()
+
+    for i in range(n-1):
+        count += ans_list[i]
+        
+    print(count)
+ 
+if __name__ == '__main__':
+    main()
