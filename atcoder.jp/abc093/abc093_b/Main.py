d3661b3e2a235b2f758745be3f46967607c6274d
kota0501 <mcz-0604-htt@outlook.com>
2020-07-12 19:49:47 +0900

    [AC] abc093 abc093_b

---
+++ atcoder.jp/abc093/abc093_b/Main.py
@@ -0,0 +1,50 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    a, b, k = i_map()
+
+    ans_list = []
+
+    for i in range(a,a+k):
+        if i > b:
+            continue
+        ans_list.append(i)
+
+    for i in range(b-k+1,b+1):
+        if i < a:
+            continue
+        ans_list.append(i)
+
+    ans_list = list(set(ans_list))
+
+    for num in ans_list:
+        print(num)
+
+if __name__ == '__main__':
+    main()
