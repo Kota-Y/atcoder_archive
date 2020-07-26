db360702f2de960a86e2cd9d6f0321e435755ba0
kota0501 <mcz-0604-htt@outlook.com>
2020-07-27 00:39:28 +0900

    [AC] abc072 arc082_a

---
+++ atcoder.jp/abc072/arc082_a/Main.py
@@ -0,0 +1,47 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    num_list = i_list()
+
+    tmp_list = [0] * 10 ** 5
+
+    for i in range(n):
+        tmp_list[num_list[i]] += 1
+
+    count = 0
+
+    for i in range(10**5-2):
+        tmp = tmp_list[i] + tmp_list[i+1] + tmp_list[i+2]
+        count = max(count, tmp)
+
+    print(count)
+
+if __name__ == '__main__':
+    main()
