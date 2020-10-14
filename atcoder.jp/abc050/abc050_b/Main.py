830499dc15e2d8126828b1b3c69499b706666ee3
kota0501 <mcz-0604-htt@outlook.com>
2020-10-15 00:41:08 +0900

    [AC] abc050 abc050_b

---
+++ atcoder.jp/abc050/abc050_b/Main.py
@@ -0,0 +1,43 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    t_list = i_list()
+    m = i_input()
+
+    sum_count = sum(t_list)
+
+    for _ in range(m):
+        p, x = i_map()
+        tmp = t_list[p-1] - x
+        print(sum_count-tmp)
+
+
+if __name__ == '__main__':
+    main()
