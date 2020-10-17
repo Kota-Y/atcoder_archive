63b8b4ebd5e22f510dc5229e17d5ca6fb49b82dd
kota0501 <mcz-0604-htt@outlook.com>
2020-10-17 20:35:38 +0900

    [AC] abc180 abc180_d

---
+++ atcoder.jp/abc180/abc180_d/Main.py
@@ -0,0 +1,54 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    x, y, a, b = i_map()
+
+    count = 0
+    while x < y:
+        if x * a < x + b:
+            x *= a
+            count += 1
+        else:
+            break
+
+    if x+b >= y:
+        if x >= y:
+            print(count-1)
+        else:
+            print(count)
+        exit()
+
+    tmp = y - x
+    plus = tmp // b
+    flag = 1 if tmp % b == 0 else 0
+
+    print(count+plus-flag)
+
+if __name__ == '__main__':
+    main()
