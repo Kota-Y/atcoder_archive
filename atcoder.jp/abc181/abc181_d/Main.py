c44b80ffc69fb6b1314f1d90a2fa85f788cefb85
kota0501 <mcz-0604-htt@outlook.com>
2020-11-01 21:41:54 +0900

    [AC] abc181 abc181_d

---
+++ atcoder.jp/abc181/abc181_d/Main.py
@@ -0,0 +1,78 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    s = s_input()
+
+    ans_list = []
+    s_co = Counter(s)
+
+    if len(s) == 1:
+        if s == '8':
+            print('Yes')
+        else:
+            print('No')
+        exit()
+
+    if len(s) < 3:
+        for i in range(10, 100):
+            if '0' in str(i):
+                continue
+            if i % 8 == 0:
+                ans_list.append(i)
+
+        for ans in ans_list:
+            ans_co = Counter(str(ans))
+            for k, v in ans_co.items():
+                if v > s_co[k]:
+                    break
+            else:
+                print('Yes')
+                exit()
+        print('No')
+        exit()
+
+    for i in range(100, 1000):
+        if '0' in str(i):
+            continue
+        if i % 8 == 0:
+            ans_list.append(i)
+    
+    for ans in ans_list:
+        ans_co = Counter(str(ans))
+        for k, v in ans_co.items():
+            if v > s_co[k]:
+                break
+        else:
+            print('Yes')
+            exit()
+        
+    print('No')
+
+if __name__ == '__main__':
+    main()
