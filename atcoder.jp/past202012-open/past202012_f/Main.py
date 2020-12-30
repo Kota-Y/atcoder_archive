e83cb9981a1eb5789e7727a86978f29c9bab39ec
kota0501 <mcz-0604-htt@outlook.com>
2020-12-31 01:54:20 +0900

    [AC] past202012-open past202012_f

---
+++ atcoder.jp/past202012-open/past202012_f/Main.py
@@ -0,0 +1,55 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n, m = i_map()
+    num_list = i_row_list(m)
+
+    tmp_list = []
+    ans = 0
+    for bit in product([0,1], repeat=n):
+        checked = [0] * n
+        for num in num_list:
+            tmp = bit[num[0]-1] + bit[num[1]-1] + bit[num[2]-1]
+            if tmp == 2:
+                if bit[num[0]-1] == 0:
+                    checked[num[0]-1] = 1
+                if bit[num[1]-1] == 0:
+                    checked[num[1]-1] = 1
+                if bit[num[2]-1] == 0:
+                    checked[num[2]-1] = 1
+            elif tmp == 3:
+                break
+        else:
+            ans = max(ans, sum(checked))
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
