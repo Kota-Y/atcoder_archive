eee7c42f035bfa1bd70414a6d6eeea7e4091373d
kota0501 <mcz-0604-htt@outlook.com>
2021-02-13 01:25:52 +0900

    [AC] abc043 arc059_b

---
+++ atcoder.jp/abc043/arc059_b/Main.py
@@ -0,0 +1,51 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    s = s_input()
+
+    first = s[0]
+    second = s[1]
+    if first == second:
+        print(1, 2)
+        exit()
+
+    for i in range(2, len(s)):
+        if s[i] == first:
+            print(i-2+1, i+1)
+            exit()
+        if s[i] == second:
+            print(i-2+1, i+1)
+            exit()
+        first = second
+        second = s[i]
+
+    print(-1, -1)
+
+if __name__ == '__main__':
+    main()
