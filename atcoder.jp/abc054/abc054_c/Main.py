795d21f2e363446d39c9d3df61bc4d30d500652c
kota0501 <mcz-0604-htt@outlook.com>
2021-02-23 02:50:27 +0900

    [AC] abc054 abc054_c

---
+++ atcoder.jp/abc054/abc054_c/Main.py
@@ -0,0 +1,51 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n, m = i_map()
+    num_list = i_row_list(m)
+
+    node_list = [i+1 for i in range(n)]
+    
+    ans = 0
+
+    for nodes in permutations(node_list):
+        if nodes[0] != 1:
+            continue
+        for i in range(1, n):
+            tmp1 = [nodes[i-1], nodes[i]]
+            tmp2 = [nodes[i], nodes[i-1]]
+            if tmp1 not in num_list and tmp2 not in num_list:
+                break
+        else:
+            ans += 1
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
