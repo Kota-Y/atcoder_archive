94839abd589a3449f7bb8a778c5ec0923a62181c
kota0501 <mcz-0604-htt@outlook.com>
2021-03-24 00:28:10 +0900

    [AC] ddcc2020-qual ddcc2020_qual_a

---
+++ atcoder.jp/ddcc2020-qual/ddcc2020_qual_a/Main.py
@@ -0,0 +1,54 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    a, b = i_map()
+    
+    ans = 0
+
+    if a == 1:
+        ans += 3
+    elif a == 2:
+        ans += 2
+    elif a == 3:
+        ans += 1
+
+    if b  == 1:
+        ans += 3
+    elif b == 2:
+        ans += 2
+    elif b == 3:
+        ans += 1
+
+    if a == 1 and b == 1:
+        ans += 4
+
+    print(ans*100000)
+
+if __name__ == '__main__':
+    main()
