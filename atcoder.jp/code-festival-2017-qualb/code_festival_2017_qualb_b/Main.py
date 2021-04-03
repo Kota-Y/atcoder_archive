4a2a6a7b1235c46f18692038211ee8da578c2a4e
kota0501 <mcz-0604-htt@outlook.com>
2021-04-03 22:36:49 +0900

    [AC] code-festival-2017-qualb code_festival_2017_qualb_b

---
+++ atcoder.jp/code-festival-2017-qualb/code_festival_2017_qualb_b/Main.py
@@ -0,0 +1,50 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    num_list = i_list()
+    m = i_input()
+    m_list = i_list()
+
+    n_co = Counter(num_list)
+    m_co = Counter(m_list)
+
+    for k, v in m_co.items():
+        if k in n_co:
+            if v > n_co[k]:
+                print('NO')
+                exit()
+        else:
+            print('NO')
+            exit()
+
+    print('YES')
+    
+if __name__ == '__main__':
+    main()
