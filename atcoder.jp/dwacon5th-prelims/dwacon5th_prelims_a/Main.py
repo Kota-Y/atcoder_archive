d6cf0131cc3de0577efe48644def1f4fb2082853
kota0501 <mcz-0604-htt@outlook.com>
2021-04-05 02:33:55 +0900

    [AC] dwacon5th-prelims dwacon5th_prelims_a

---
+++ atcoder.jp/dwacon5th-prelims/dwacon5th_prelims_a/Main.py
@@ -0,0 +1,50 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    num_list = i_list()
+
+    sum_num = sum(num_list)
+
+    ave_num = sum_num / n
+
+    ans_list = []
+
+    for i in range(n):
+        ans_list.append(abs(ave_num-num_list[i]))
+
+    min_num = min(ans_list)
+
+    for i in range(n):
+        if min_num == ans_list[i]:
+            print(i)
+            exit()
+
+if __name__ == '__main__':
+    main()
