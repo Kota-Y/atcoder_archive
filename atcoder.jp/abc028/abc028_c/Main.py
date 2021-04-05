fd74b35364af7d4d122030a40637ccdb480c4949
kota0501 <mcz-0604-htt@outlook.com>
2021-04-06 00:05:26 +0900

    [AC] abc028 abc028_c

---
+++ atcoder.jp/abc028/abc028_c/Main.py
@@ -0,0 +1,49 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    num_list = i_list()
+
+    ans_list = set()
+    for bit in product([0,1], repeat=5):
+        if bit.count(1) != 3:
+            continue
+        tmp_sum = 0
+        for i in range(5):
+            if bit[i] != 1:
+                continue
+            tmp_sum += num_list[i]
+        ans_list.add(tmp_sum)
+
+    ans_list = list(ans_list)
+    ans_list.sort(reverse=True)
+
+    print(ans_list[2])
+
+if __name__ == '__main__':
+    main()
