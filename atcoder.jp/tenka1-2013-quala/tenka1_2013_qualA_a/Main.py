188585202a1234459049a3abc633b0cf078d635f
kota0501 <mcz-0604-htt@outlook.com>
2021-04-16 00:08:06 +0900

    [AC] tenka1-2013-quala tenka1_2013_qualA_a

---
+++ atcoder.jp/tenka1-2013-quala/tenka1_2013_qualA_a/Main.py
@@ -0,0 +1,11 @@
+def main():
+    ans = 42
+
+    for i in range(1000):
+        if ans > 130_000_000:
+            print(ans)
+            break
+        ans *= 2
+    slice
+if __name__ == '__main__':
+    main()
