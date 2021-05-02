68e1d03d2a2343a5e1405ed6bb5777e02050f803
kota0501 <mcz-0604-htt@outlook.com>
2021-05-02 20:46:56 +0900

    [AC] jsc2021 jsc2021_b

---
+++ atcoder.jp/jsc2021/jsc2021_b/Main.py
@@ -0,0 +1,52 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n, m = i_map()
+    n_list = i_list()
+    m_list = i_list()
+    
+    n_set = set(n_list)
+    m_set = set(m_list)
+
+    ans_list = []
+
+    for i in range(n):
+        if n_list[i] not in m_set:
+            ans_list.append(n_list[i])
+
+    for i in range(m):
+        if m_list[i] not in n_set:
+            ans_list.append(m_list[i])
+
+    ans_list.sort()
+
+    print(*ans_list)
+
+if __name__ == '__main__':
+    main()
