0f88c5a93b5ea48246dc13e9acbc294bca46e342
kota0501 <mcz-0604-htt@outlook.com>
2021-05-04 12:43:07 +0900

    [AC] typical90 typical90_p

---
+++ atcoder.jp/typical90/typical90_p/Main.py
@@ -0,0 +1,50 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    a, b, c = i_map()
+    
+    ans = INF
+
+    for i in range(10000):
+        if a*i > n:
+            continue
+        for j in range(10000):
+            tmp = n-a*i-b*j
+            if tmp < 0:
+                continue
+            if tmp % c != 0:
+                continue
+            if ans > i+j+tmp//c:
+                ans = min(ans, i+j+tmp//c)
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
