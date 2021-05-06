ec1830011f902f82694cef93d1e25902f16f3412
kota0501 <mcz-0604-htt@outlook.com>
2021-05-06 18:32:03 +0900

    [AC] yahoo-procon2019-qual yahoo_procon2019_qual_c

---
+++ atcoder.jp/yahoo-procon2019-qual/yahoo_procon2019_qual_c/Main.py
@@ -0,0 +1,50 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    k, a, b = i_map()
+
+    if a >= b:
+        print(k+1)
+        exit()
+
+    ans = a
+    k -= a - 1
+    
+    if k == 1:
+        print(ans+1)
+    elif b - a == 1:
+        print(ans+k)
+    else:
+        if k % 2 == 0:
+            print(ans+(b-a)*(k//2))
+        else:
+            print(ans+(b-a)*(k//2)+1)
+
+if __name__ == '__main__':
+    main()
