35d0a24432a4d296ff35727711c2deaaa9984153
kota0501 <mcz-0604-htt@outlook.com>
2021-05-11 00:32:55 +0900

    [AC] tenka1-2017-beginner tenka1_2017_b

---
+++ atcoder.jp/tenka1-2017-beginner/tenka1_2017_b/Main.py
@@ -0,0 +1,46 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    num_list = i_row_list(n)
+    
+    num_list.sort()
+
+    max_no = num_list[0][0]
+    max_socre = num_list[0][1]
+    min_no = num_list[n-1][0]
+    min_socre = num_list[n-1][1]
+
+    ans = min_no + min_socre
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
