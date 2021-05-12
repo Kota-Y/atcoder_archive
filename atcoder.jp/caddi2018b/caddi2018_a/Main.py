3bc36efe52faf54735df414495d969248f9a6e80
kota0501 <mcz-0604-htt@outlook.com>
2021-05-13 00:23:25 +0900

    [AC] caddi2018b caddi2018_a

---
+++ atcoder.jp/caddi2018b/caddi2018_a/Main.py
@@ -0,0 +1,61 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd
+from copy import deepcopy
+from collections import Counter, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def prime_factorize(n: int) -> list:
+    return_list = []
+    while n % 2 == 0:
+        return_list.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            return_list.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        return_list.append(n)
+    return return_list
+
+def main():
+    n, p = i_map()
+    
+    prime_list = prime_factorize(p)
+
+    prime_co = Counter(prime_list)
+
+    ans = 1
+
+    for k,v in prime_co.items():
+        for i in range(v//n):
+            ans *= k
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
