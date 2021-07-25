a1f893f5182bdf70ea5d0d3dee7d5b37e7458e70
kota0501 <mcz-0604-htt@outlook.com>
2021-07-26 00:26:56 +0900

    [AC] typical90 typical90_bw

---
+++ atcoder.jp/typical90/typical90_bw/Main.py
@@ -0,0 +1,58 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def prime_factorize(n: int) -> list:
+    return_list = []
+    while n % 2 == 0:
+        return_list.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            return_list.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        return_list.append(n)
+    return return_list
+
+def main():
+    n = i_input()
+
+    res = prime_factorize(n)
+
+    for i in range(50):
+        if pow(2, i) >= len(res):
+            print(i)
+            exit()
+    
+if __name__ == '__main__':
+    main()
