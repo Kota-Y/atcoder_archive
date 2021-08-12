ea80f527716e8c9064cd9192e439b8cca4d548b9
kota0501 <mcz-0604-htt@outlook.com>
2021-08-12 18:54:55 +0900

    [AC] past202005-open past202005_i

---
+++ atcoder.jp/past202005-open/past202005_i/Main.py
@@ -0,0 +1,63 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    q = i_input()
+
+    x_list = [i for i in range(n)]
+    y_list = [i for i in range(n)]
+
+    reversed_flag = False
+
+    for _ in range(q):
+        str_list = s_map()
+        if str_list[0] == '1':
+            a, b = int(str_list[1]), int(str_list[2])
+            if reversed_flag:
+                y_list[a-1], y_list[b-1] = y_list[b-1], y_list[a-1]
+            else:
+                x_list[a-1], x_list[b-1] = x_list[b-1], x_list[a-1]
+        elif str_list[0] == '2':
+            a, b = int(str_list[1]), int(str_list[2])
+            if reversed_flag:
+                x_list[a-1], x_list[b-1] = x_list[b-1], x_list[a-1]
+            else:
+                y_list[a-1], y_list[b-1] = y_list[b-1], y_list[a-1]
+        elif str_list[0] == '3':
+            reversed_flag = not reversed_flag
+        elif str_list[0] == '4':
+            a, b = int(str_list[1]), int(str_list[2])
+            if reversed_flag:
+                a, b = b, a
+            print(n * x_list[a-1] + y_list[b-1])
+
+if __name__ == '__main__':
+    main()
