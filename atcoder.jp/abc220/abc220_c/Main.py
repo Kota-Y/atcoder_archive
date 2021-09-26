856e7313f2c313e4b3ccce1c469048e7087f3193
kota0501 <mcz-0604-htt@outlook.com>
2021-09-26 21:19:48 +0900

    [AC] abc220 abc220_c

---
+++ atcoder.jp/abc220/abc220_c/Main.py
@@ -0,0 +1,60 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    num_list = i_list()
+    x = i_input()
+
+    sum_l = sum(num_list)
+
+    ans = x // sum_l
+    amari = x % sum_l
+
+    if ans != 0:
+        nokori = 0
+        for i in range(n):
+            nokori += num_list[i]
+            if nokori > amari:
+                break
+        print(ans*n+max(i+1,1))
+    else:
+        nokori = 0
+        for i in range(n):
+            if nokori > x:
+                print(i)
+                exit()
+            nokori += num_list[i]
+
+        print(n)
+
+if __name__ == '__main__':
+    main()
