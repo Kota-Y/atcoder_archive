7b9743fa5a5eecca8c30d1f471ea7b52e7a81614
kota0501 <mcz-0604-htt@outlook.com>
2021-10-03 15:34:58 +0900

    [AC] typical90 typical90_cg

---
+++ atcoder.jp/typical90/typical90_cg/Main.py
@@ -0,0 +1,60 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+# 約数を列挙
+def make_divisors(n: int) -> list:
+    return_list = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            return_list.append(i)
+            if i != n // i:
+                return_list.append(n//i)
+        
+    return return_list
+
+def main():
+    k = i_input()
+
+    yakusu_list = make_divisors(k)
+    yakusu_list.sort()
+
+    ans = 0
+    for a in yakusu_list:
+        for b in yakusu_list:
+            c = k / (a*b)
+            if a <= b <= c: 
+                if c.is_integer():
+                    ans += 1
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
