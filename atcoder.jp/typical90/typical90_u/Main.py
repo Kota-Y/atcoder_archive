8379f6ad262391691c6f07850df994c3b9124bf9
kota0501 <mcz-0604-htt@outlook.com>
2021-10-14 00:58:23 +0900

    [AC] typical90 typical90_u

---
+++ atcoder.jp/typical90/typical90_u/Main.py
@@ -0,0 +1,113 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+class SCC:
+    def __init__(self, n):
+        self.n = n
+        self.graph = [[] for _ in range(n)]
+        self.rev_graph = [[] for _ in range(n)]
+        self.labels = [-1] * n
+        self.lb_cnt = 0
+
+    def add_edge(self, v, nxt_v):
+        self.graph[v].append(nxt_v)
+        self.rev_graph[nxt_v].append(v)
+
+    def build(self):
+        self.post_order = []
+        self.used = [False] * self.n
+        for v in range(self.n):
+            if not self.used[v]:
+                self._dfs(v)
+        for v in reversed(self.post_order):
+            if self.labels[v] == -1:
+                self._rev_dfs(v)
+                self.lb_cnt += 1
+
+    def _dfs(self, v):
+        stack = [v, 0]
+        while stack:
+            v, idx = stack[-2:]
+            if not idx and self.used[v]:
+                stack.pop()
+                stack.pop()
+            else:
+                self.used[v] = True
+                if idx < len(self.graph[v]):
+                    stack[-1] += 1
+                    stack.append(self.graph[v][idx])
+                    stack.append(0)
+                else:
+                    stack.pop()
+                    self.post_order.append(stack.pop())
+
+    def _rev_dfs(self, v):
+        stack = [v]
+        self.labels[v] = self.lb_cnt
+        while stack:
+            v = stack.pop()
+            for nxt_v in self.rev_graph[v]:
+                if self.labels[nxt_v] != -1:
+                    continue
+                stack.append(nxt_v)
+                self.labels[nxt_v] = self.lb_cnt
+
+    def construct(self):
+        self.dag = [[] for i in range(self.lb_cnt)]
+        self.groups = [[] for i in range(self.lb_cnt)]
+        for v, lb in enumerate(self.labels):
+            for nxt_v in self.graph[v]:
+                nxt_lb = self.labels[nxt_v]
+                if lb == nxt_lb:
+                    continue
+                self.dag[lb].append(nxt_lb)
+            self.groups[lb].append(v)
+        return self.dag, self.groups
+
+def main():
+    n, m = i_map()
+
+    scc = SCC(n)
+    for _ in range(m):
+        a, b = i_map()
+        scc.add_edge(a-1, b-1)
+
+    scc.build()
+    dags, groups = scc.construct()
+
+    ans = 0
+    for i in range(len(groups)):
+        ans += len(groups[i]) * (len(groups[i])-1) // 2
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
