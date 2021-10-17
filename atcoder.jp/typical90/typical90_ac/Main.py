2a4ee2be1eb3d3149e41e7488f69155a85e2055b
kota0501 <mcz-0604-htt@outlook.com>
2021-10-17 17:46:01 +0900

    [AC] typical90 typical90_ac

---
+++ atcoder.jp/typical90/typical90_ac/Main.py
@@ -0,0 +1,140 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+"""
+〜segfuncの使い方について〜
+update(k, x): k番目の要素をxに更新する
+query(l, r): [l, r)（l <= k < r の区間）から値kを取得する
+"""
+def segfunc(x: int, y: int) -> int:
+    "ここで求めたい処理を行う, max(x, y) や x ^ y など"
+    return max(x, y)
+
+"""
+〜単位元の一覧について〜
+最小値：float("inf")
+最大値：-float("inf")
+XOR：0
+区間和：0
+区間積：1
+最大公約数：0
+"""
+ide_ele = 0 # 初期値（単位元）の設定
+
+class LazySegmentTree:
+    def __init__(self, init_val, segfunc, ide_ele):
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.data = [ide_ele] * 2 * self.num
+        self.lazy = [None] * 2 * self.num
+        for i in range(n):
+            self.data[self.num + i] = init_val[i]
+        for i in range(self.num - 1, 0, -1):
+            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1])
+
+    def gindex(self, l, r):
+        l += self.num
+        r += self.num
+        lm = l >> (l & -l).bit_length()
+        rm = r >> (r & -r).bit_length()
+        while l < r:
+            if l <= lm:
+                yield l
+            if r <= rm:
+                yield r
+            r >>= 1
+            l >>= 1
+        while l:
+            yield l
+            l >>= 1
+
+    def propagates(self, *ids):
+        for i in reversed(ids):
+            v = self.lazy[i]
+            if v is None:
+                continue
+            self.lazy[2 * i] = v
+            self.lazy[2 * i + 1] = v
+            self.data[2 * i] = v
+            self.data[2 * i + 1] = v
+            self.lazy[i] = None
+
+    def update(self, l, r, x):
+        *ids, = self.gindex(l, r)
+        self.propagates(*ids)
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                self.lazy[l] = x
+                self.data[l] = x
+                l += 1
+            if r & 1:
+                self.lazy[r - 1] = x
+                self.data[r - 1] = x
+            r >>= 1
+            l >>= 1
+        for i in ids:
+            self.data[i] = self.segfunc(self.data[2 * i], self.data[2 * i + 1])
+
+    def query(self, l, r):
+        *ids, = self.gindex(l, r)
+        self.propagates(*ids)
+        res = self.ide_ele
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.data[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.data[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
+
+def main():
+    w, n = i_map()
+
+    height = [0] * w
+
+    seg = LazySegmentTree(height, segfunc, ide_ele)
+
+    for _ in range(n):
+        l, r = i_map()
+        cnt = seg.query(l-1, r)
+        seg.update(l-1, r, cnt+1)
+        print(cnt+1)    
+
+if __name__ == '__main__':
+    main()
