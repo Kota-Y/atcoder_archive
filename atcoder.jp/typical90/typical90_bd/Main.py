2c2b5d915331de04d85b0951a13f3a07ab06771b
kota0501 <mcz-0604-htt@outlook.com>
2021-10-25 01:26:39 +0900

    [AC] typical90 typical90_bd

---
+++ atcoder.jp/typical90/typical90_bd/Main.py
@@ -0,0 +1,66 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n, s = i_map()
+    num_list = i_row_list(n)
+
+    dp = [[False] * (s + 1) for _ in range(n + 1)]
+    dp[0][0] = True
+
+    for i in range(n):
+        for j in range(s+1):
+            if j - num_list[i][0] >= 0:
+                dp[i+1][j] |= dp[i][j-num_list[i][0]]
+            if j - num_list[i][1] >= 0:
+                dp[i+1][j] |= dp[i][j-num_list[i][1]]
+
+    if not dp[n][s]:
+        print('Impossible')
+        exit()
+
+    ans = ""
+    for i in reversed(range(n)):
+        Bool = True
+        if s - num_list[i][0] >= 0:
+            if dp[i][s - num_list[i][0]] == True:
+                ans += "A"
+                s -= num_list[i][0]
+                Bool = False
+        if s - num_list[i][1] >= 0 and Bool:
+            if dp[i][s - num_list[i][1]] == True:
+                ans += "B"
+                s -= num_list[i][1]
+
+    print(ans[::-1])
+
+if __name__ == '__main__':
+    main()
