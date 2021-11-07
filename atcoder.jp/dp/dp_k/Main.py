ee8f020a52ab12b0028200f158210ade214859d0
kota0501 <mcz-0604-htt@outlook.com>
2021-11-08 01:26:46 +0900

    [AC] dp dp_k

---
+++ atcoder.jp/dp/dp_k/Main.py
@@ -0,0 +1,46 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n, k = i_map()
+    num_list = i_list()
+    
+    dp = [0] * (k+1)
+
+    for i in range(1, k+1):
+        for j in range(1, n+1):
+            if i - num_list[j-1] >= 0 and dp[i-num_list[j-1]] == 0:
+                dp[i] = 1
+
+    print('First' if dp[k] else 'Second')
+
+if __name__ == '__main__':
+    main()
