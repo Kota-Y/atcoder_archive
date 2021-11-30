bb24d30a1bd2cdfb7391242e4736a886369776de
kota0501 <mcz-0604-htt@outlook.com>
2021-12-01 01:04:32 +0900

    [AC] code-festival-2015-qualb codefestival_2015_qualB_c

---
+++ atcoder.jp/code-festival-2015-qualb/codefestival_2015_qualB_c/Main.py
@@ -0,0 +1,60 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n, m = i_map()
+    a_list = i_list()
+    b_list = i_list()
+
+    if n < m:
+        print('NO')
+        exit()
+
+    a_list.sort(reverse=True)
+    b_list.sort(reverse=True)
+
+    a_index = 0
+    flag = False
+    for i in range(m):
+        while b_list[i] > a_list[a_index]:
+            a_index += 1
+            flag = True
+            if a_index == n:
+                print('NO')
+                exit()
+        if not flag:
+            a_index += 1
+        flag = False
+
+    print('YES')
+
+if __name__ == '__main__':
+    main()
