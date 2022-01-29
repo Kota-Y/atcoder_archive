ec7c977baca19ddcfe14446286cb28a43936a504
kota0501 <mcz-0604-htt@outlook.com>
2022-01-29 14:57:34 +0900

    [AC] abc091 arc092_a

---
+++ atcoder.jp/abc091/arc092_a/Main.py
@@ -0,0 +1,61 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import ceil, floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    n = i_input()
+    r_list = i_row_list(n)
+    b_list = i_row_list(n)
+    
+    r_list.sort()
+    b_list.sort()
+
+    used_r_list = [0] * n
+
+    co = 0
+
+    for c, d in b_list:
+        tmp_index = -1
+        tmp_max_y = -1
+        for i, (a, b) in enumerate(r_list):
+            if used_r_list[i]:
+                continue
+            if c > a and d > b:
+                if tmp_max_y < b:
+                    tmp_index = i
+                    tmp_max_y = b
+        if tmp_index != -1:
+            used_r_list[tmp_index] = 1
+            co += 1
+
+    print(co)
+
+if __name__ == '__main__':
+    main()
