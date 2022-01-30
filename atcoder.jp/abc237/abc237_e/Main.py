ef2188e828029deaceaeae6b08f5c1abef130bd2
kota0501 <mcz-0604-htt@outlook.com>
2022-01-31 00:32:55 +0900

    [AC] abc237 abc237_e

---
+++ atcoder.jp/abc237/abc237_e/Main.py
@@ -0,0 +1,74 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import floor, sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+def ceil(a, b): return (a + b - 1) // b
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def dijkstra(start: int, node_num: int, edge: list) -> list:
+    d = [float("inf")] * node_num
+    d[start] = 0
+    #スタートをキューに入れる
+    q = [(0,start)]
+
+    while len(q) != 0:
+        #キューの先頭を取得
+        ci, i = heappop(q)
+        if d[i] < ci:
+            continue
+        #キューの先頭から繋がっている頂点を探索
+        for cj, j in edge[i]:
+            if d[j] > d[i] + cj:
+                d[j] = d[i] + cj
+                heappush(q, (d[j], j))
+
+    return d
+
+def main():
+    n, m = i_map()
+    h_list = i_list()
+    num_list = i_row_list(m)
+  
+    edge = [[] for _ in range(n)]
+    for u, v in num_list:
+        u_h = h_list[u-1]
+        v_h = h_list[v-1]
+
+        edge[u-1].append((max(0, v_h-u_h), v-1))
+        edge[v-1].append((max(0, u_h-v_h), u-1))
+
+    res = dijkstra(0, n, edge)
+
+    ans = 0
+    for i in range(n):
+        ans = max(ans, h_list[0] - res[i] - h_list[i])
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
