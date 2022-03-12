0006863c13f7e811157b1925c6cab8edd4ac53c7
kota0501 <mcz-0604-htt@outlook.com>
2022-03-12 21:04:11 +0900

    [AC] abc243 abc243_a

---
+++ atcoder.jp/abc243/abc243_a/Main.py
@@ -0,0 +1,54 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+def ceil(a, b): return (a + b - 1) // b
+def floor(a, b): return a // b
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def main():
+    v, a, b, c = i_map()
+    
+    for i in range(v+10):
+        if i % 3 == 0:
+            v -= a
+        elif i % 3 == 1:
+            v -= b
+        else:
+            v -= c
+        
+        if v < 0:
+            break
+
+    if i % 3 == 0:
+        print('F')
+    elif i % 3 == 1:
+        print('M')
+    else:
+        print('T')
+
+if __name__ == '__main__':
+    main()
