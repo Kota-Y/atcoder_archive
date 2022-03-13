5f67b39c949ae33fc86a4e8cb3ffa6121e9fc8db
kota0501 <mcz-0604-htt@outlook.com>
2022-03-13 18:59:38 +0900

    [AC] abc242 abc242_c

---
+++ atcoder.jp/abc242/abc242_c/Main.py
@@ -0,0 +1,64 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+def ceil(a, b): return (a + b - 1) // b
+def floor(a, b): return a // b
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 998244353
+
+def main():
+    n = i_input()
+    
+    dp = [[0] * 9 for i in range(n)]
+
+    for i in range(9):
+        dp[0][i] = 1
+
+    for i in range(1, n):
+        for j in range(9):
+            if j - 1 < 0:
+                plus1 = 0
+            else:
+                plus1 = dp[i-1][j-1]
+
+            if j + 1 >= 9:
+                plus2 = 0
+            else:
+                plus2 = dp[i-1][j+1]
+
+            dp[i][j] += dp[i-1][j] % MOD
+            dp[i][j] += plus1 % MOD
+            dp[i][j] += plus2 % MOD
+
+    ans = 0
+    for i in range(9):
+        ans += dp[n-1][i]
+        ans %= MOD
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
