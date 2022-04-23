bc9505b3b0775151ac25c178560f98cefb38279a
kota0501 <mcz-0604-htt@outlook.com>
2022-04-24 03:40:52 +0900

    [AC] abc249 abc249_d

---
+++ atcoder.jp/abc249/abc249_d/Main.py
@@ -0,0 +1,62 @@
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+def ceil(a, b): return (a + b - 1) // b
+def floor(a, b): return a // b
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+def make_divisors(n: int) -> list:
+    return_list = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            return_list.append(i)
+            if i != n // i:
+                return_list.append(n//i)
+
+    return return_list
+
+def main():
+    n = i_input()
+    num_list = i_list()
+
+    d = defaultdict(int)
+    for num in num_list:
+        d[num] += 1
+
+    ans = 0
+
+    for i in range(n):
+        results = make_divisors(num_list[i])
+        for res in results:
+            j = d[res]
+            k = d[num_list[i] // res]
+            ans += j * k
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
