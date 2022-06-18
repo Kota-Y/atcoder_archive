7c4a2ee4dfad45961b4bcd9eb1f474a6b4557746
kota0501 <mcz-0604-htt@outlook.com>
2022-06-18 22:16:46 +0900

    [AC] abc256 abc256_e

---
+++ atcoder.jp/abc256/abc256_e/Main.py
@@ -0,0 +1,123 @@
+from ctypes import Union
+import sys, re
+from string import ascii_lowercase, ascii_uppercase, digits, hexdigits, octdigits
+from operator import add, sub, mul, mod, xor
+from math import sqrt, pi, factorial, gcd, sin, cos, atan2,degrees, radians
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce
+from decimal import Decimal, getcontext
+from array import array
+# input = sys.stdin.readline 
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+def lcm(a, b): return a * b // gcd(a, b)
+def ceil(a, b): return (a + b - 1) // b
+def floor(a, b): return a // b
+sys.setrecursionlimit(10 ** 6)
+INF = float('inf')
+MOD = 10 ** 9 + 7
+
+class UnionFind:
+    # 作りたい要素数nで初期化
+    # 使用するインスタンス変数の初期化
+    def __init__(self, n):
+        self.n = n
+        # root[x]<0ならそのノードが根かつその値が木の要素数
+        # rootノードでその木の要素数を記録する
+        self.root = [-1]*(n+1)
+        # 木をくっつける時にアンバランスにならないように調整する
+        self.rank = [0]*(n+1)
+
+    # ノードxのrootノードを見つける
+    def findRoot(self, x):
+        if(self.root[x] < 0): # 根
+            return x
+        else:
+            # ここで代入しておくことで、後の繰り返しを避ける
+            self.root[x] = self.findRoot(self.root[x])
+            return self.root[x]
+
+    # 木の併合、入力は併合したい各ノード
+    def unite(self, x, y):
+        # 入力ノードのrootノードを見つける
+        x = self.findRoot(x)
+        y = self.findRoot(y)
+
+        # すでに同じ木に属していた場合
+        if x == y:
+            return 
+
+        # 違う木に属していた場合rankを見てくっつける方を決める
+        if self.rank[x] > self.rank[y]:
+            self.root[x] += self.root[y]
+            self.root[y] = x
+
+        else:
+            self.root[y] += self.root[x]
+            self.root[x] = y
+            # rnkが同じ（深さに差がない場合）は1増やす
+            if self.rank[x] == self.rank[y]:
+                self.rank[y] += 1
+
+    # xとyが同じグループに属するか判断
+    def isSameGroup(self, x, y):
+        return self.findRoot(x) == self.findRoot(y)
+
+    # ノードxが属する木のサイズを返す
+    def count(self, x):
+        return -self.root[self.findRoot(x)]
+
+    # グループ数をカウント
+    def groupCount(self):
+        count = 0
+        for i in range(1, self.n+1):
+            if self.root[i] < 0: 
+                count += 1
+        return count
+
+def main():
+    n = i_input()
+    x_list = i_list()
+    c_list = i_list()
+
+    edges = []
+    l = [-1] * 10 ** 6
+    for i in range(n):
+        edges.append([i+1, x_list[i]])
+        l[i+1] = x_list[i]
+
+    uf = UnionFind(n)
+
+    ans_cost = 0
+    for a, b in edges:
+        if uf.isSameGroup(a, b):
+            start = b
+            finish = -2
+            cost_l = []
+            index = start
+            while start != finish:
+                index = l[index]
+                finish = index
+                cost_l.append(c_list[index-1])
+            cost_l.sort()
+            ans_cost += cost_l[0]
+        else:
+            uf.unite(a, b)
+
+    print(ans_cost)
+
+if __name__ == '__main__':
+    main()
